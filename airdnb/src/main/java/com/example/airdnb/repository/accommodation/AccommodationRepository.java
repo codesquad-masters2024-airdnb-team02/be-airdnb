28477dd0cc13fdfce55e6bd601a49343067ce635
Keon Kim <110909423+keon0711@users.noreply.github.com>
2024-06-13 13:34:24 +0900

    Revert "Booking 생성, 조회 API 구현"

--- airdnb/src/main/java/com/example/airdnb/repository/accommodation/AccommodationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.airdnb.repository.accommodation;
-
-import com.example.airdnb.domain.accommodation.Accommodation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccommodationRepository extends JpaRepository<Accommodation, Long> {
-
-}
--- airdnb/src/main/java/com/example/airdnb/repository/booking/BookingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.airdnb.repository.booking;
-
-
-import com.example.airdnb.domain.booking.Booking;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookingRepository extends JpaRepository<Booking, Long> {
-
-}
