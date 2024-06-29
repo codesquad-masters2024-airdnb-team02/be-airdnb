e701bd2b732be7633dac6124a9686c86384eca9f
shimbaa <shim9597@gmail.com>
2024-06-29 16:06:11 +0900

    :bug: fix: 컴파일 오류 수정

--- airdnb/src/main/java/com/example/airdnb/repository/jpa/AccommodationRepository.java
+++ airdnb/src/main/java/com/example/airdnb/repository/jpa/AccommodationRepository.java
@@ -1,6 +1,7 @@
 package com.example.airdnb.repository.jpa;
 
 import com.example.airdnb.domain.accommodation.Accommodation;
+import com.example.airdnb.repository.AccommodationRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccommodationRepository extends JpaRepository<Accommodation, Long>, AccommodationRepositoryCustom {
